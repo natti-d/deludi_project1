c68279fe185f849387bb03ee9d59ab8ff7a177da
Student <lachezar668@gmail.com>
2023-11-07 14:03:16 +0200

    new js

--- script.js
+++ script.js
@@ -58,3 +58,63 @@ function changeKarta() {
     vhod.classList.remove("btn-outline-success")
 
 }
+
+function changeRegistraciq() {
+    let registraciq = document.getElementById('registraciq')
+    registraciq.style.color = "#99D19C"
+    registraciq.style.borderBlockColor = "#99D19C"
+    console.log("changed 2")
+    registraciq.classList.remove("btn-success")
+    registraciq.classList.add("btn-outline-success")
+
+    let obekti = document.getElementById('obekti')
+    obekti.style.removeProperty("color")
+    obekti.style.borderBlockColor = null
+    obekti.classList.add("btn-success")
+    obekti.classList.remove("btn-outline-success")
+
+    let karta = document.getElementById('karta')
+    karta.style.removeProperty("color")
+    karta.style.removeProperty("border-color")
+    karta.style.borderBlockColor = null
+    karta.classList.add("btn-success")
+    karta.classList.remove("btn-outline-success")
+
+    let vhod = document.getElementById('vhod')
+    vhod.style.removeProperty("color")
+    vhod.style.removeProperty("border-color")
+    vhod.style.borderBlockColor = null
+    vhod.classList.add("btn-success")
+    vhod.classList.remove("btn-outline-success")
+
+}
+
+function changeVhod() {
+    let vhod = document.getElementById('vhod')
+    vhod.style.color = "#99D19C"
+    vhod.style.borderBlockColor = "#99D19C"
+    console.log("changed 2")
+    vhod.classList.remove("btn-success")
+    vhod.classList.add("btn-outline-success")
+
+    let obekti = document.getElementById('obekti')
+    obekti.style.removeProperty("color")
+    obekti.style.borderBlockColor = null
+    obekti.classList.add("btn-success")
+    obekti.classList.remove("btn-outline-success")
+
+    let registraciq = document.getElementById('registraciq')
+    registraciq.style.removeProperty("color")
+    registraciq.style.removeProperty("border-color")
+    registraciq.style.borderBlockColor = null
+    registraciq.classList.add("btn-success")
+    registraciq.classList.remove("btn-outline-success")
+
+    let karta = document.getElementById('karta')
+    karta.style.removeProperty("color")
+    karta.style.removeProperty("border-color")
+    karta.style.borderBlockColor = null
+    karta.classList.add("btn-success")
+    karta.classList.remove("btn-outline-success")
+
+}
