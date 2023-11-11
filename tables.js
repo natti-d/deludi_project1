4bad86e293aeeffffee150c79ba8f48063182abc
Nataliq <nat.g.dimitrova@gmail.com>
2023-11-11 21:35:35 +0200

    started js

---
+++ tables.js
@@ -0,0 +1,42 @@
+let locations = [{
+    "id": 0,
+    "BG":[{
+        "name_of_object_BG": "Белоградчишки скали",
+        "info_BG": "Белоградчишките скали са едно от природните чудеса на България и едно от най-живописните места в страната. Тези необичайни червеникави скални скулптури могат да бъдат намерени в северозападна България.\nПовечето от тях приличат на животни или хора и има много легенди около различните герои.През годините местните жители са развихрили въображението си, създавайки безброй легенди за скалите, фокусирани върху тяхната прилика с животински и човешки фигури.Близнаците, Ученичката, Овчарката, Мечката, Конникът и Мадоната обикновено са най- известни.\nБелоградчишките скали са включени в списъка на световното наследство на ЮНЕСКО.",
+        "location_BG": "Белоградчишките скали се простират от запад на изток от връх Ведерник около Белоградчик до село Белотинци. Общата им дължина е 30 километра, а общата им ширина е 6-7 километра. Разстоянието между центъра на града и Белоградчишките скали при Белоградчишката крепост \\\"Калето\\\" е 1 км. Можете да ходите пеша или да карате там. В близост до Белоградчишките скали (крепост) има паркинг.",
+    }],
+    "EN":[{
+        "name_of_object_EN": "Belogradchik Rocks",
+        "info_EN": "The Belogradchik rocks are one of Bulgaria's natural wonders and one of the most picturesque spots in the country. These unusual reddish rock sculptures can be found in northwestern Bulgaria.\nThe majority of them resemble animals or people, and there are numerous legends surrounding the various characters.Over the years, locals have let their imaginations run wild, making countless legends about the rocks focused on their resemblance to animal and human figures.The Twins, the Schoolgirl, the Shepherdess, the Bear, the Horseman, and the Madonna tend to be the most well- known.\nThe Belogradchik rocks are included as a UNESCO World Heritage site.",
+        "location_EN": "The Belogradchik rocks extend from west to east, from Mount Vedernik around Belogradchik to the village of Belotintsi. Their overall length is 30 kilometres, while their total width is 6-7 kilometres. The distance between the city centre and the Belogradchik rocks at the Belogradchik fortress \\\"Kaleto\\\" is 1 km. You can either walk or drive there. A parking area is located near to the Belogradchishki rocks (fortress)."
+    }]    
+},
+{
+    "id": 1,
+    "BG":[{
+        "name_of_object_BG": "Копривщица",
+        "info_BG": "Копривщица е известна със своята историческа българска архитектура, народни празници и исторически забележителности.\nКопривщица представлява един от най- добре запазените възрожденски градове в страната, отразяващ същността и атмосферата на епохата на българското Възраждане.Историята на този град е неразривно свързана с въстанията срещу Османската империя през 19 век, поради което той заема важно място в сърцата на много българи.\nКаменният мост на Първа пушка, наред с къщите - музеи на Димчо Дебелянов, Любен Каравелов, Тодор Каблешков, Георги Бенковски и други бележити български личности и революционери, са сред най - значимите забележителности на Копривщица.\nОт 1965 г.на всеки пет години тук се провежда Националният събор на българското народно творчество, а през 2025 г.ще се проведе 12 - ото му издание.Събитието събира музиканти, танцьори и художници от всички краища на страната, за да представят своето творчество в продължение на период от три дни.",
+        "location_BG": "",
+    }],
+    "EN": [{
+        "name_of_object_EN": "Koprivshtitsa",
+        "info_EN": "Koprivshtitsa is well-known for its historic Bulgarian architecture, folk festivals, and historical landmarks.\nKoprivshtitsa represents one of the best- preserved Renaissance towns in the country, reflecting the essence and atmosphere of the Bulgarian Renaissance period.This town's history has an unbreakable connection to the 19th-century uprisings against the Ottoman Empire, which is why it occupies an important place in the hearts of numerous Bulgarians.\nThe stone bridge of the First Rifle, alongside the house - museums of Dimcho Debelyanov, Lyuben Karavelov, Todor Kableshkov, Georgi Benkovski, and other significant Bulgarian personas and revolutionaries, belong to the most significant attractions in Koprivshtitsa.\nThe National Assembly of Bulgarian Folk Art has been held here every five years since 1965, and the 12th edition will be taking place in 2025. The event gathers together musicians, dancers, and artists from every part of the country to present their work over a period of three days.",
+        "location_EN": ""
+    }]
+},
+
+//Шаблон
+    {
+        "id": 0,
+        "BG": [{
+            "name_of_object_BG": "",
+            "info_BG": "",
+            "location_BG": "",
+        }],
+        "EN": [{
+            "name_of_object_EN": "",
+            "info_EN": "",
+            "location_EN": ""
+        }]
+    },
+];
