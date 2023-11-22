d958b6b2bc88668d3423af08e7583e8a8655b06e
Student <nat.g.dimitrova@gmail.com>
2023-11-22 12:41:01 +0200

    added table

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+var info_locations = [
+    {
+        "id": 0,
+        "BG": [{
+            "name_of_object": "Белоградчишки скали",
+            "info": "Белоградчишките скали са едно от природните чудеса на България и едно от най-живописните места в страната. Тези необичайни червеникави скални скулптури могат да бъдат намерени в северозападна България.\nПовечето от тях приличат на животни или хора и има много легенди около различните герои.През годините местните жители са развихрили въображението си, създавайки безброй легенди за скалите, фокусирани върху тяхната прилика с животински и човешки фигури.Близнаците, Ученичката, Овчарката, Мечката, Конникът и Мадоната обикновено са най- известни.\nБелоградчишките скали са включени в списъка на световното наследство на ЮНЕСКО.",
+            "location": "Белоградчишките скали се простират от запад на изток от връх Ведерник около Белоградчик до село Белотинци. Общата им дължина е 30 километра, а общата им ширина е 6-7 километра. Разстоянието между центъра на града и Белоградчишките скали при Белоградчишката крепост \\\"Калето\\\" е 1 км. Можете да ходите пеша или да карате там. В близост до Белоградчишките скали (крепост) има паркинг.",
+        },
+    ],
+        "EN": [{
+            "name_of_object": "Belogradchik Rocks",
+            "info": "The Belogradchik rocks are one of Bulgaria's natural wonders and one of the most picturesque spots in the country. These unusual reddish rock sculptures can be found in northwestern Bulgaria.\nThe majority of them resemble animals or people, and there are numerous legends surrounding the various characters.Over the years, locals have let their imaginations run wild, making countless legends about the rocks focused on their resemblance to animal and human figures.The Twins, the Schoolgirl, the Shepherdess, the Bear, the Horseman, and the Madonna tend to be the most well- known.\nThe Belogradchik rocks are included as a UNESCO World Heritage site.",
+            "location": "The Belogradchik rocks extend from west to east, from Mount Vedernik around Belogradchik to the village of Belotintsi. Their overall length is 30 kilometres, while their total width is 6-7 kilometres. The distance between the city centre and the Belogradchik rocks at the Belogradchik fortress \\\"Kaleto\\\" is 1 km. You can either walk or drive there. A parking area is located near to the Belogradchishki rocks (fortress)."
+        }]
+    },
+    {
+        "id": 1,
+        "BG": [{
+            "name_of_object": "Копривщица",
+            "info": "Копривщица е известна със своята историческа българска архитектура, народни празници и исторически забележителности.\nКопривщица представлява един от най- добре запазените възрожденски градове в страната, отразяващ същността и атмосферата на епохата на българското Възраждане.Историята на този град е неразривно свързана с въстанията срещу Османската империя през 19 век, поради което той заема важно място в сърцата на много българи.\nКаменният мост на Първа пушка, наред с къщите - музеи на Димчо Дебелянов, Любен Каравелов, Тодор Каблешков, Георги Бенковски и други бележити български личности и революционери, са сред най - значимите забележителности на Копривщица.\nОт 1965 г.на всеки пет години тук се провежда Националният събор на българското народно творчество, а през 2025 г.ще се проведе 12 - ото му издание.Събитието събира музиканти, танцьори и художници от всички краища на страната, за да представят своето творчество в продължение на период от три дни.",
+            "location": "Копривщица се намира на 12 km от Подбалканския път, източно от столицата София (на 108 km) и от Пирдоп (на 27 km), северно от Пловдив (87 km) и Стрелча (21 km). Градът е разположен в живописната долина по течението на река Тополница, в планински район, централна част на Същинска Средна гора.",
+        }],
+        "EN": [{
+            "name_of_object": "Koprivshtitsa",
+            "info": "Koprivshtitsa is well-known for its historic Bulgarian architecture, folk festivals, and historical landmarks.\nKoprivshtitsa represents one of the best- preserved Renaissance towns in the country, reflecting the essence and atmosphere of the Bulgarian Renaissance period.This town's history has an unbreakable connection to the 19th-century uprisings against the Ottoman Empire, which is why it occupies an important place in the hearts of numerous Bulgarians.\nThe stone bridge of the First Rifle, alongside the house - museums of Dimcho Debelyanov, Lyuben Karavelov, Todor Kableshkov, Georgi Benkovski, and other significant Bulgarian personas and revolutionaries, belong to the most significant attractions in Koprivshtitsa.\nThe National Assembly of Bulgarian Folk Art has been held here every five years since 1965, and the 12th edition will be taking place in 2025. The event gathers together musicians, dancers, and artists from every part of the country to present their work over a period of three days.",
+            "location": "Koprivshtitsa is located 12 km from the Sub-Balkan road, east of the capital Sofia (108 km) and Pirdop (27 km), north of Plovdiv (87 km) and Strelcha (21 km). The city is located in the picturesque valley along the Topolnitsa River, in a mountainous area, central part of Sštinska Sredna Gora."
+        }]
+    },
+    {
+        "id": 2,
+        "BG": [{
+            "name_of_object": "Крушунски водопади",
+            "info": "Крушунските водопади без съмнение са едно от най-привлекателните и очарователни места за посещение в България. Освен това са лесно достъпни - с автомобил. Освен това те могат да бъдат заобиколени по сравнително кратка екопътека. В резултат на това те се превърнаха в една от най-популярните природни забележителности у нас през последните няколко години.\nПо същество Крушунските водопади са най-значимата бигорна каскада в България. Село Крушуна е разположено в източната част на Деветашкото плато, в силно карстова околност. Там се намира местността \\\"Маарата\\\", където попадат и водопадите. Думата \\\"Маара\\\" буквално означава \\\"пещера\\\".",
+            "location": "Крушунски водопади са поредица от водопади в близост до село Крушуна, Община Летница, Област Ловеч, Северна България. Намират се на 8 км южно от град Летница, на 1 км южно от село Крушуна и са известни със своята живописност. Водопадите представляват дълга травертинова (бигорна) каскада, отделните стъпала на която са самите водопади.",
+        }],
+        "EN": [{
+            "name_of_object": "Krushuna Waterfalls",
+            "info": "The Krushun waterfalls are beyond a doubt one of the most appealing and fascinating places to visit in Bulgaria. Furthermore, they are easily accessible - by vehicle. Additionally, they can be bypassed by following a relatively short eco-trail. As a result, they have come to be one of the most popular natural attractions in our country in the past few years.\nIn essence, the Krushun waterfalls are Bulgaria's most significant travertine cascade. The village of Krushuna lies on the eastern side of the Devetasha Plateau, in a very karst surroundings. The \\\"Maarata\\\" location, where the waterfalls also fall, is located there. The word \\\"Maara\\\" literally means \\\"cave\\\".",
+            "location": "Krushuna Waterfalls are a series of waterfalls near the village of Krushuna, Letnitsa Municipality, Lovech Region, Northern Bulgaria. They are located 8 km south of the town of Letnitsa, 1 km south of the village of Krushuna and are known for their picturesqueness. The falls are a long travertine (magenta) cascade, the individual steps of which are the falls themselves."
+        }]
+    }
+
+    //Шаблон
+    /*{
+        "id": 0,
+        "BG": [{
+            "name_of_object": "",
+            "info": "",
+            "location": "",
+        }],
+        "EN": [{
+            "name_of_object": "",
+            "info": "",
+            "location": ""
+        }]
+    },*/
+];
+
+var bg_lang = true;
+
 function changeObekti() {
     activating('obekti')
     deactivating('vhod')
