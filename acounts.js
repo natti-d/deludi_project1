98a1e5174d008ebe89c09d80abe0b27d87fd5b24
Student <lachezar668@gmail.com>
2023-12-13 13:01:11 +0200

    acount.js

---
+++ acounts.js
@@ -0,0 +1,11 @@
+var acounts = [
+{
+    "id": 0,
+    "username": "admin",
+    "email": "admin@gmail.com",
+    "password": "admin"
+
+},
+];
+const button = document.getElementById('password');
+button.addEventListener("input",function(){ alert("too long"); })
