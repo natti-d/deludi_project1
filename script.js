f8ea3c57aa2bae131336ccb3d6d7cedcf8a2d8cd
Student <nat.g.dimitrova@gmail.com>
2023-12-13 11:12:59 +0200

    images

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var info_locations = [
             "name_of_object": "Белоградчишки скали",
             "info": "Белоградчишките скали са едно от природните чудеса на България и едно от най-живописните места в страната. Тези необичайни червеникави скални скулптури могат да бъдат намерени в северозападна България.\nПовечето от тях приличат на животни или хора и има много легенди около различните герои. През годините местните жители са развихрили въображението си, създавайки безброй легенди за скалите, фокусирани върху тяхната прилика с животински и човешки фигури.Близнаците, Ученичката, Овчарката, Мечката, Конникът и Мадоната обикновено са най-известни.\nБелоградчишките скали са включени в списъка на световното наследство на ЮНЕСКО.",
             "location": "Белоградчишките скали се простират от запад на изток от връх Ведерник около Белоградчик до село Белотинци. Общата им дължина е 30 километра, а общата им ширина е 6-7 километра. Разстоянието между центъра на града и Белоградчишките скали при Белоградчишката крепост \\\"Калето\\\" е 1 км. Можете да ходите пеша или да карате там. В близост до Белоградчишките скали (крепост) има паркинг.",
-            "image": "./image/belogradchik/0.png",
         },
         ],
         "EN": [{
@@ -13,6 +12,7 @@ var info_locations = [
             "info": "The Belogradchik rocks are one of Bulgaria's natural wonders and one of the most picturesque spots in the country. These unusual reddish rock sculptures can be found in northwestern Bulgaria.\nThe majority of them resemble animals or people, and there are numerous legends surrounding the various characters.Over the years, locals have let their imaginations run wild, making countless legends about the rocks focused on their resemblance to animal and human figures.The Twins, the Schoolgirl, the Shepherdess, the Bear, the Horseman, and the Madonna tend to be the most well- known.\nThe Belogradchik rocks are included as a UNESCO World Heritage site.",
             "location": "The Belogradchik rocks extend from west to east, from Mount Vedernik around Belogradchik to the village of Belotintsi. Their overall length is 30 kilometres, while their total width is 6-7 kilometres. The distance between the city centre and the Belogradchik rocks at the Belogradchik fortress \\\"Kaleto\\\" is 1 km. You can either walk or drive there. A parking area is located near to the Belogradchishki rocks (fortress)."
         }],
+        "image": "./image/belogradchik/0.png",
         "url": "https://www.google.com/maps/dir/?api=1&destination=43.6233471,22.6753521&travelmode=driving"
     },
     {
@@ -317,7 +317,7 @@ function Load() {
 
         let object_image = document.createElement("img");
         object_image.classList.add("card-img-top", "img-fluid");
-        object_image.src = info_locations[i].BG[0].image;
+        object_image.src = info_locations[i].image;
         object_div2.appendChild(object_image);
 
         let object_div3 = document.createElement("div");
