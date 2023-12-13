49c3dfdd7a09077d434f8394f3d9b3f356996807
Student <lachezar668@gmail.com>
2023-12-13 11:49:14 +0200

    neshto

--- script.js
+++ script.js
@@ -21,13 +21,15 @@ var info_locations = [
             "name_of_object": "Копривщица",
             "info": "Копривщица е известна със своята историческа българска архитектура, народни празници и исторически забележителности.\nКопривщица представлява един от най- добре запазените възрожденски градове в страната, отразяващ същността и атмосферата на епохата на българското Възраждане.Историята на този град е неразривно свързана с въстанията срещу Османската империя през 19 век, поради което той заема важно място в сърцата на много българи.\nКаменният мост на Първа пушка, наред с къщите - музеи на Димчо Дебелянов, Любен Каравелов, Тодор Каблешков, Георги Бенковски и други бележити български личности и революционери, са сред най - значимите забележителности на Копривщица.\nОт 1965 г.на всеки пет години тук се провежда Националният събор на българското народно творчество, а през 2025 г.ще се проведе 12 - ото му издание.Събитието събира музиканти, танцьори и художници от всички краища на страната, за да представят своето творчество в продължение на период от три дни.",
             "location": "Копривщица се намира на 12 km от Подбалканския път, източно от столицата София (на 108 km) и от Пирдоп (на 27 km), северно от Пловдив (87 km) и Стрелча (21 km). Градът е разположен в живописната долина по течението на река Тополница, в планински район, централна част на Същинска Средна гора.",
-            "image": "./image/koprivshtica/1.png",
+            
         }],
         "EN": [{
             "name_of_object": "Koprivshtitsa",
             "info": "Koprivshtitsa is well-known for its historic Bulgarian architecture, folk festivals, and historical landmarks.\nKoprivshtitsa represents one of the best- preserved Renaissance towns in the country, reflecting the essence and atmosphere of the Bulgarian Renaissance period.This town's history has an unbreakable connection to the 19th-century uprisings against the Ottoman Empire, which is why it occupies an important place in the hearts of numerous Bulgarians.\nThe stone bridge of the First Rifle, alongside the house - museums of Dimcho Debelyanov, Lyuben Karavelov, Todor Kableshkov, Georgi Benkovski, and other significant Bulgarian personas and revolutionaries, belong to the most significant attractions in Koprivshtitsa.\nThe National Assembly of Bulgarian Folk Art has been held here every five years since 1965, and the 12th edition will be taking place in 2025. The event gathers together musicians, dancers, and artists from every part of the country to present their work over a period of three days.",
             "location": "Koprivshtitsa is located 12 km from the Sub-Balkan road, east of the capital Sofia (108 km) and Pirdop (27 km), north of Plovdiv (87 km) and Strelcha (21 km). The city is located in the picturesque valley along the Topolnitsa River, in a mountainous area, central part of Sštinska Sredna Gora."
-        }]
+        }],
+        "image": "./image/koprivshtica/1.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=42.63585847256458,24.35926561314669&travelmode=driving"
     },
     {
         "id": 2,
@@ -41,7 +43,9 @@ var info_locations = [
             "name_of_object": "Krushuna Waterfalls",
             "info": "The Krushun waterfalls are beyond a doubt one of the most appealing and fascinating places to visit in Bulgaria. Furthermore, they are easily accessible - by vehicle. Additionally, they can be bypassed by following a relatively short eco-trail. As a result, they have come to be one of the most popular natural attractions in our country in the past few years.\nIn essence, the Krushun waterfalls are Bulgaria's most significant travertine cascade. The village of Krushuna lies on the eastern side of the Devetasha Plateau, in a very karst surroundings. The \\\"Maarata\\\" location, where the waterfalls also fall, is located there. The word \\\"Maara\\\" literally means \\\"cave\\\".",
             "location": "Krushuna Waterfalls are a series of waterfalls near the village of Krushuna, Letnitsa Municipality, Lovech Region, Northern Bulgaria. They are located 8 km south of the town of Letnitsa, 1 km south of the village of Krushuna and are known for their picturesqueness. The falls are a long travertine (magenta) cascade, the individual steps of which are the falls themselves."
-        }]
+        }],
+        "image": "./image/krushunski vodopadi/1.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=43.24337254773605,25.03318846438476&travelmode=driving"
     },
     {
         "id": 3,
@@ -49,13 +53,15 @@ var info_locations = [
             "name_of_object": "Арбанаси",
             "info": "Арбанаси е една от най-популярните дестинации и красиви места в България. В това малко селце има множество хотели и ресторанти.Една от най-добрите атракции е църквата „\\\Рождество Христово\\\“.\nВ него има няколко красиви стенописи, датиращи от 16 и 17 век. Можете също така да посетите една от типичните къщи в стила на Възраждането – Констанцалиевата къща – и да научите как хората са живели в онези дни.",
             "location": "Арбанаси е много малко село, кацнало на хълм на около 5 километра от Велико Търново. \nОсвен невероятната гледка към Велико Търново и Царевец, Арбанаси предлага и старомодни калдъръмени улички и стари къщи, заобиколени от високи възрожденски каменни огради.",
-            "image": "./image/arbanasi/0.png"
+            
         }],
         "EN": [{
             "name_of_object": "Arbanasi",
             "info": "Arbanasi is one of the most popular destinations and beautiful places in Bulgaria. There are many hotels and restaurants in this small village. One of the best attractions is the Church of the Nativity. It has several beautiful frescoes dating from the 16th and 17th centuries. You can also visit one of the typical houses of the Revival style - Constantaliev House - and learn how people lived in those days.",
             "location": "Arbanasi is a very small village perched on a hill about 5 kilometers from Veliko Tarnovo. \nBesides the spectacular view of Veliko Tarnovo and Tsarevets, Arbanasi also offers old-fashioned cobbled streets and old houses surrounded by high Renaissance stone fences."
-        }]
+        }],
+        "image": "./image/arbanasi/0.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=43.09892732965902,25.669114842369922&travelmode=driving"
     },
     {
         "id": 4,
@@ -63,13 +69,15 @@ var info_locations = [
             "name_of_object": "Залив Болата",
             "info": "На Болата е изграден малък пристан, където рибарите опъват въдиците си, както и заведение, предлагащо разхладителни напитки през лятото. Плажът е чист и приятен, а водата плитка, бистра и прохладна. Заливът е отлично място за гмуркане. Това е и единствения резерват в България, включващ в пределите си и акватория (морска площ).",
             "location": "Болата е местност, разположена на едноименния залив и е част от резерват Калиакра. \nНамира се в северната част на Българското Черноморие край село Българево и в близост до бивша военна станция.",
-            "image": "./image/zaliv bolata/0.png"
+            
         }],
         "EN": [{
             "name_of_object": "Gulf Bolata",
             "info": "A small pier has been built on Bolata, where fishermen stretch their fishing rods, as well as a restaurant offering refreshing drinks in the summer. The beach is clean and pleasant, and the water is shallow, clear and cool. The bay is an excellent place for diving. This is also the only reserve in Bulgaria, including within its boundaries a water area (sea area).",
             "location": "Bolata is a locality located on the bay of the same name and is part of the Kaliakra reserve. \nIt is located in the northern part of the Bulgarian Black Sea near the village of Bulgarevo and near a former military station."
-        }]
+        }],
+        "image": "./image/zaliv bolata/0.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=43.38291871411453,28.469223233818642&travelmode=driving"
     },
     {
         "id": 5,
@@ -77,13 +85,15 @@ var info_locations = [
             "name_of_object": "Дяволският мост",
             "info": "Мостът представлява една прекрасна дестинация за еднодневна разходка сред природата с кратък планински преход. В близост е изградено и обособено място за отдих и пикник, което пък би било чудесно допълнение към разходката. ",
             "location": "Разположен над река Арда и сгушен в прегръдката на Родопите, се извисява Дяволският мост. Той датира от началото на 19-ти век, а с името му са свързани немалко местни легенди. ",
-            "image": "./image/dqvolski most/7.png"
+           
         }],
         "EN": [{
             "name_of_object": "The Devil's Bridge",
             "info": "The bridge is a wonderful destination for a one-day walk in nature with a short mountain hike. Nearby, there is also a designated place for recreation and picnic, which would be a great addition to the walk.",
             "location": "Located above the Arda River and nestled in the embrace of the Rhodopes, towers the Devil's Bridge. It dates back to the beginning of the 19th century, and many local legends are associated with its name."
-        }]
+        }],
+        "image": "./image/dqvolski most/7.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=41.620619918877146,25.114086351441838&travelmode=driving"
     },
     {
         "id": 6,
@@ -91,13 +101,15 @@ var info_locations = [
             "name_of_object": "Перперикон",
             "info": "Перперикон представлява каменен комплекс от останки от ранната история, античността и средновековието. Смята се, че мястото е било населено още през 5000 г. пр. Хр., като част от обитателите му са били траките, римляните, готите и ромеите. Комплексът се състои от голямо мегалитно светилище, паметници от антични и средновековни крепости, катедрали и много други.",
             "location": "Скалният град Перперек, ограден от три страни от реката, се намира на 6 км западно от село Перперек, на 2 км южно от село Горна крепост и на 4 км източно от село Стремци.",
-            "image": "./image/perperikon/8.png"
+           
         }],
         "EN": [{
             "name_of_object": "Perperikon",
             "info": "Perperikon is a stone complex of remains from early history, antiquity and the Middle Ages. It is believed that the place was inhabited as early as 5000 BC, with Thracians, Romans, Goths and Romai among its inhabitants.The complex consists of a large megalithic sanctuary, monuments of ancient and medieval fortresses, cathedrals and much more.",
             "location": "The rock town of Perperek, surrounded on three sides by the river, is located 6 km west of the village of Perperek, 2 km south of the village of Gorna Krepost and 4 km east of the village of Stremtsi."
-        }]
+        }],
+        "image": "./image/perperikon/8.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=41.71522003157851,25.466604914114416&travelmode=driving"
     },
     {
         "id": 7,
@@ -105,13 +117,15 @@ var info_locations = [
             "name_of_object": "Резиденция Евксиноград",
             "info": "Резиденция Евксиноград е една от най-романтичните сгради в България и едно от най-добрите места за посещение в България.Външността на имението много прилича на френските имения от 18-ти век. Резиденцията е богато украсена, романтична и доста красива. Интериорът е луксозен, с мраморни камини, френски полилеи, големи трапезарии, антични мебели и редки картини. Очаквайте да видите много злато и коприна!",
             "location": "Разположен на 8 километра северно от Варна, този дворец от 19-ти век често е бил използван като ваканционен дом за кралски особи. Сградата е построена, след като принц Батенберг посети Варна през 1880г. и беше толкова впечатлен от местната природа, че трябваше да има имот в района.",
-            "image": "./image/rezidenciq/9.png"
+           
         }],
         "EN": [{
             "name_of_object": "Residence Euxinograd",
             "info": "Euxinograd Residence is one of the most romantic buildings in Bulgaria and one of the best places to visit in Bulgaria. The exterior of the mansion is very similar to the French mansions of the 18th century. The residence is ornate, romantic and quite beautiful. The interior is luxurious, with marble fireplaces, French chandeliers, large dining rooms, antique furniture and rare paintings. Expect to see lots of gold and silk!",
             "location": "Located 8 kilometers north of Varna, this 19th-century palace was often used as a holiday home for royalty. The building was built after Prince Battenberg visited Varna in 1880. and was so impressed by the local scenery that he had to own a property in the area."
-        }]
+        }],
+        "image": "./image/rezidenciq/9.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=43.21937022193307,27.99535591366512&travelmode=driving"
     },
     {
         "id": 8,
@@ -119,13 +133,15 @@ var info_locations = [
             "name_of_object": "Рилския манастир",
             "info": "Първоначално е основан през 10 век от св. Йоан Рилски, отшелник живял в близката пещера. Стотици поклонници и туристи идват да посещават мястото всяка година и от векове той е бил от изключително духовно значение.",
             "location": "Разположен е в Югозападна България, област Кюстендил, община Рила.",
-            "image": "./image/rilski manastir/10.png"
+           
         }],
         "EN": [{
             "name_of_object": "The Rila Monastery",
             "info": "It was originally founded in the 10th century by St. John of Rila, a hermit who lived in a nearby cave. Hundreds of pilgrims and tourists come to visit the site every year and it has been of great spiritual importance for centuries.",
             "location": "It is located in Southwest Bulgaria, Kyustendil district, Rila municipality."
-        }]
+        }],
+        "image": "./image/rilski manastir/10.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=42.133578247129456,23.340846356458897&travelmode=driving"
     },
     {
         "id": 9,
@@ -133,13 +149,15 @@ var info_locations = [
             "name_of_object": "Пловдив",
             "info": "Пловдив се смята за един от най-старите градове в света, както и най-старият все още населен град в Европа, по-древен дори от Атина. Това може да означава само едно – невероятно богата история, римски руини, красива стара архитектура и несравнима атмосфера. ",
             "location": "Той е разположен по двата бряга на р. Марица в Горнотракийската низина на 42° и 9′ северна ширина и 24° и 45′ източна дължина, а най-ниската му точка е на 160 м надморска височина. Територията му е 101,981 кв. км, а населението на града е около 338 153 души.",
-            "image": "./image/plovdiv/11.png"
+           
         }],
         "EN": [{
             "name_of_object": "Plovdiv",
             "info": "Plovdiv is considered one of the oldest cities in the world, as well as the oldest still inhabited city in Europe, older even than Athens. This can only mean one thing – an incredibly rich history, Roman ruins, beautiful old architecture and an unparalleled atmosphere.",
             "location": "It is located on both banks of the Maritsa River in the Upper Thracian Plain at 42° and 9′ north latitude and 24° and 45′ east longitude, and its lowest point is 160 m above sea level. Its territory is 101,981 sq. km, and the population of the city is about 338,153 people."
-        }]
+        }],
+        "image": "./image/plovdiv/11.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=42.13322283522093,24.74651133439881&travelmode=driving"
     },
     {
         "id": 10,
@@ -147,13 +165,15 @@ var info_locations = [
             "name_of_object": "Варна",
             "info": "Една от най-добрите летни дестинации в страната, Варна често е наричана морската столица на България. През лятото той се превръща в оживен град с много дейности и фестивали, които да забавляват гостите на града.",
             "location": "Ва̀рна е най-големият град в Североизточна България, разположен по бреговете на Черно море и Варненското езеро и е административен център на едноименните община Варна·",
-            "image": "./image/varna/12.png"
+           
         }],
         "EN": [{
             "name_of_object": "Varna",
             "info": "One of the best summer destinations in the country, Varna is often called the sea capital of Bulgaria. In the summer, it becomes a lively city with many activities and festivals to keep the city's guests entertained.",
             "location": "Varna is the largest city in North-Eastern Bulgaria, located on the shores of the Black Sea and Lake Varna and is the administrative center of the municipality of the same name, Varna."
-        }]
+        }],
+        "image": "./image/varna/12.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=43.214070145268344,27.914753679216243&travelmode=driving"
     },
     {
         "id": 11,
@@ -161,13 +181,15 @@ var info_locations = [
             "name_of_object": "Седемте рилски езера",
             "info": "Седемте рилски езера са група ледникови езера и местните жители са им дали имена, вдъхновени най-вече от формата им: Окото, Бъбрекът, Сълзата, Близнакът, Трилистникът, Рибното езеро и Долното езеро. Езерата са разположени в Национален парк Рила, най-големият национален парк в България.",
             "location": "Разположено в северозападната част на Рила планина, седемте рилски езера са зона със зашеметяваща красота и всеки посетил това място е съгласен, че те са част от най-красивите места в България.",
-            "image": "./image/ezera/13.png"
+            
         }],
         "EN": [{
             "name_of_object": "Тhe seven Rila lakes",
             "info": "The seven Rila lakes are a group of glacial lakes and the local inhabitants gave them names inspired mostly by their shape: the Eye, the Kidney, the Tear, the Twin, the Shamrock, the Fish Lake and the Lower Lake. The lakes are located in Rila National Park, the largest national park in Bulgaria.",
             "location": "Located in the northwestern part of the Rila Mountains, the seven Rila lakes are an area of ​​stunning beauty and everyone who has visited this place agrees that they are some of the most beautiful places in Bulgaria."
-        }]
+        }],
+        "image": "./image/ezera/13.png",
+        "url": "https://www.google.com/maps/dir/?api=1&destination=42.203381005679816,23.323261527250764&travelmode=driving"
     }
 
     //Шаблон
