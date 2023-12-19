fee61b3e7a4b474377f7013fc71768de16ff3ca4
Student <lachezar668@gmail.com>
2023-12-19 14:46:54 +0200

    jidajew

--- acounts.js
+++ acounts.js
@@ -11,4 +11,42 @@ var acounts = [
 const button = document.getElementById('password');
 button.addEventListener("input",function(){ alert("too long"); })
 
+const loginForm = document.getElementById('Login_id');
+loginForm.addEventListener('submit', Validation);
 
+function Validation(event)
+{
+    event.preventDefault();
+
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    if(!validateEmail(email.value))
+    {
+        alert('Please enter correct email!');
+        return;
+    }
+    if(!validatePass(password.value))
+    {
+        alert('Please enter correct email!');
+        return;
+    }
+
+    alert('Log in is Successful')
+}
+
+function vaidateEmail()
+{
+    let conditions = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return conditions.test(email);
+}
+
+function validatePass()
+{
+    let minLenght = 8;
+    let HasUppercase = /[A-Z]/.test(password);
+    let HasLowercase = /[a-z]/.test(password);
+    let HasNumbers = /[0-9]/.test(password);
+
+    return (password.Lenght >= minLenght && HasUppercase && HasLowercase && HasNumbers);
+}
